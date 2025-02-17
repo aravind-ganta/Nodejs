a72d8c19fada7fefef22fbe5821de477337aa0ba
aravind-ganta <gantaaravind1196@gmail.com>
2025-02-17 23:46:40 +0530

    REST API code added

---
+++ project-1/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs=require("fs");
+const app = express();
+const PORT = 8000;
+//Middleware- Plugin
+app.use(express.urlencoded({extended:false}));
+
+
+//Routes
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+  res.send(html);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //ToDO: Edit the user with id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    //ToDO: Delete the user with id
+    return res.json({ status: "pending" });
+  });
+
+app.post("/api/users", (req, res) => {
+  //ToDO: Create new user
+  const body= req.body;
+  users.push({...body, id: users.length+1});
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
+    return res.json({status:"success", id:users.length});
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started at PORT:${PORT}`);
+});

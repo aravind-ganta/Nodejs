31acfe2d7b61ba75e4c961b9212cb8892528434d
aravind-ganta <gantaaravind1196@gmail.com>
2025-02-18 17:11:39 +0530

    added middlewares, http headers, status codes

--- project-1/index.js
+++ project-1/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
-const fs=require("fs");
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
 //Middleware- Plugin
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 //Routes
 app.get("/api/users", (req, res) => {
+  // console.log(req.headers);
+
+  // Always add X to custom headers
+  res.setHeader("X-MyName", "Aravind");
+
   return res.json(users);
 });
 
@@ -26,6 +40,7 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) return res.status(404).json({error: "user not found"});
     return res.json(user);
   })
   .patch((req, res) => {
@@ -39,10 +54,13 @@ app
 
 app.post("/api/users", (req, res) => {
   //ToDO: Create new user
-  const body= req.body;
-  users.push({...body, id: users.length+1});
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
-    return res.json({status:"success", id:users.length});
+  const body = req.body;
+  if(!body || !body.first_name || !body.email || !body.gender || !body.job_title ){
+    return res.status(400).json({msg: "All fields are required..."});
+  }
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
